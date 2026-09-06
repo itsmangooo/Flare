210ee39f88b0cf142723817cd9444d84731206fc
mango <vjecni1@gmail.com>
2026-09-06 11:51:17 +0200

    Remove legacy C# backend

--- src/Flare.Api/Data/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Flare.Api.Data;
-
-public sealed class ApplicationUser : IdentityUser<Guid>
-{
-    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
-    public ICollection<RefreshToken> RefreshTokens { get; set; } = [];
-}
